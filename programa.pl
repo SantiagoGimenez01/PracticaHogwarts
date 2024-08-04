b632db01b8562805d31303e00b737c0763055db8
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-08-04 13:24:41 -0300

    Punto 1 hecho

--- programa.pl
+++ programa.pl
@@ -1,1 +1,25 @@
-% Aquí va el código.
+% mago(Persona).
+mago(harry).
+mago(draco).
+mago(hermione).
+% caracteristicasMago(Mago, caracteristicas(Sangre, Caracteristicas, OdiaCasa)).
+caracteristicasMago(harry, caracteristicas(mestiza, [coraje, amistoso, orgullo, inteligencia], [slytherin])).
+caracteristicasMago(hermione, caracteristicas(impura, [inteligencia, orgullo, responsabilidad], [])).
+caracteristicasMago(draco, caracteristicas(pura, [inteligencia, orgullo], [hufflepuff])).
+% caracteristicaCasa(Casa, Caracteristica).
+caracteristicaCasa(gryffindor, [coraje]).
+caracteristicaCasa(slytherin, [orgullo, inteligencia]).
+caracteristicaCasa(ravenclaw, [inteligencia, responsabilidad]).
+caracteristicaCasa(hufflepuff, [amistoso]).
+% Punto 1
+% puedeEntrar(Mago, Casa).
+puedeEntrar(Mago, gryffindor):-
+    mago(Mago).
+puedeEntrar(Mago, ravenclaw):-
+    mago(Mago).
+puedeEntrar(Mago, hufflepuff):-
+    mago(Mago).
+puedeEntrar(Mago, slytherin):-
+    mago(Mago),
+    caracteristicasMago(Mago, caracteristicas(Sangre, _, _)),
+    not(Sangre = impura).
